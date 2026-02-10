c5e70b0f9e06e39588f304c426a7a174b343f29c
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-02-10 13:18:17 +0000

    feat(Nav): Built the desktop navigation for the Header
    - Added the logo and styling
    - Created the structure of the nav bar using Bootstrap for desktop.
    - Will update the layout to make sure mobile view is working

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./MainNav.css";
+
+export default function MainNav() {
+  return (
+    <nav className="navbar navbar-expand-sm p-2">
+      <div className="container">
+        <a className="navbar-brand logo" href="/">
+          VIEW
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/home">
+                HOME <span className="sr-only"></span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                ABOUT
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/theviews">
+                THE VIEWS
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/signin">
+                SIGN IN
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/signup">
+                SIGN UP
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
