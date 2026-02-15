de8ef2c27d78e2e482eaa49938498461e45b9f30
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-02-15 16:09:58 +0000

    feat(signin): Added structure to the Sign In page including images and styling with bootstrap. Only for desktop.

--- src/pages/public/SignIn.jsx
+++ src/pages/public/SignIn.jsx
@@ -3,13 +3,60 @@ import "../public/SignIn.css";
 
 export default function SignIn() {
   return (
-    <div className="card  d-flex rows p-5">
-      <div className="card-body ">
-        <div className=" p-lg-5 text-left">
-          <h1>SignIn.</h1>
-          <p className="pt-4"></p>
+    <>
+      <div className="view-textimage d-flex rows pt-5">
+        <div className="col-lg-6 d-flex columns align-items-start justify-content-start">
+          <div className="card m-5">
+            <div className="card-body m-5">
+              <h2 className="pb-5">Log In</h2>
+              <form>
+                <div clasName="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="ENTER EMAIL"
+                  />
+                </div>
+                <div className="form-group pt-2">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="PASSWORD"
+                  />
+                </div>
+                <div className="form-check pt-3 pb-3">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" for="exampleCheck1">
+                    REMEMBER ME
+                  </label>
+                </div>
+
+                <button type="submit" className="red-btn login-btn">
+                  LOG IN
+                </button>
+                <hr></hr>
+                <button type="submit" className="outline-red-btn login-btn">
+                  LOG IN WITH GOOGLE
+                </button>
+
+                <a className="register">Register here for a account</a>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 p-5">
+          <div className="card login-img p-5">
+            <div className="card-body d-flex columns align-items-end"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
