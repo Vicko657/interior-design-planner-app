98c15ff24f4ffac418c595c7fd6f5f47a428c716
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-03-03 21:29:01 +0000

    fix(mainnav): Fixed the mainnav component and added mobile responsiveness.
    - Changed the container to container-fluid to stop the nav moving when the page shrinks
    
    - Fixed the toggle and changed the type to offcanvas
    
    - Updated nav styling to work for dark and light mode

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -11,52 +11,72 @@ export default function MainNav() {
     <nav
       className={`${location === "theviews" || location === "signin" || location === "signup" ? "dark" : "light"} navbar navbar-expand-lg p-2`}
     >
-      <div className="container">
-        <a className="navbar-brand logo" href="/">
+      <div className="container-fluid">
+        <a className="navbar-brand navlogo logo" href="/">
           VIEW
         </a>
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#navbarOffcanvasLg"
+          aria-controls="navbarOffcanvasLg"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon align-content-center justify-content-center">
+            <div className="navline mb-1"></div>
+            <div className="navline"></div>
+          </span>
         </button>
         <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
+          className="offcanvas offcanvas-start"
+          tabIndex={"-1"}
+          id="navbarOffcanvasLg"
+          aria-labelledby="navbarOffcanvasLgLabel"
         >
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to="/home">
-                HOME <span className="sr-only"></span>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
-                ABOUT
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/theviews">
-                THE VIEWS
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/signin">
-                SIGN IN
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/signup">
-                SIGN UP
-              </NavLink>
-            </li>
-          </ul>
+          <div className="offcanvas-header">
+            <a className="offcanvas-title logo" href="/">
+              VIEW
+            </a>
+            <button
+              className="btn-close btn-color"
+              type="button"
+              data-bs-dismiss="offcanvas"
+              data-bs-target="#navbarOffcanvasLg"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1">
+              <li className="nav-item">
+                <NavLink className="nav-link active" to={"/"}>
+                  HOME <span className="sr-only"></span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"/about"}>
+                  ABOUT
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"/theviews"}>
+                  THE VIEWS
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"/signin"}>
+                  SIGN IN
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"/signup"}>
+                  SIGN UP
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
