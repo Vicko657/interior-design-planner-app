9bb6ebc1c5c95cfa9775eef09e38ddab5e906a29
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-05-23 19:34:51 +0100

    feat: add mock client data

---
+++ src/data/clients.js
@@ -0,0 +1,21 @@
+const clients = [
+  {
+    id: 0,
+    fullName: "John Moss",
+    emailAddress: "johnmoss@gmail.com",
+    phoneNumber: "07853394758",
+    address: "60 Belsize Avenue, London, N1 3EE",
+    totalProjects: "3",
+    notes: "Prefers morning appointments",
+  },
+  {
+    id: 1,
+    fullName: "Jessica Cook",
+    emailAddress: "jessicacook@gmail.com",
+    phoneNumber: "07314708068",
+    address: "33 Elm Street, London, N2R 652",
+    totalProjects: "1",
+    notes: "Prefers eco-friendly materials",
+  },
+];
+export default clients;
