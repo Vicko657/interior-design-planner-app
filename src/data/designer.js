4b36a6d3b6ea7ac4583c8bdd8e6c8479e85cc45c
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-05-24 23:27:17 +0100

    feat: add designer mock data

---
+++ src/data/designer.js
@@ -0,0 +1,10 @@
+const designers = [
+  {
+    id: 0,
+    fullName: "Grace Smith",
+    username: "gracesmith",
+    clients: [{ clientName: "John Moss" }, { clientName: "Jessica Cook" }],
+    role: "designer",
+  },
+];
+export default designers;
