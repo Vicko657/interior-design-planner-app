09177b14010723bc8ed3300a18a0088c28721a54
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-01 21:19:23 +0100

    feat: add user checks in private and public layout to restrict user access

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import "./PublicLayout.css";
 import Header from "./public/Header";
 import Footer from "./public/Footer";
+import { useAuth } from "../../context/AuthContext";
 
 export default function PublicLayout() {
+  const { isAuthenticated } = useAuth();
+
+  // Checks if user is logged in
+  if (isAuthenticated) {
+    return <Navigate to="/user"></Navigate>;
+  }
   return (
     <>
       {/*-- Header */}
