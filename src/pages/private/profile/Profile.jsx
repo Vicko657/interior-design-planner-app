2ba497cb9888a1abc320e6e33f5df7611c2c1ff2
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-18 19:23:43 +0100

    feat: add fetch functionality to profile page and update mobile structure

--- src/pages/private/profile/Profile.jsx
+++ src/pages/private/profile/Profile.jsx
@@ -1,16 +1,45 @@
 import { React, useEffect, useState } from "react";
+import designerService from "../../../api/services/designerService";
 import "./Profile.css";
 import Button from "../../../components/common/Button";
+import { useForm } from "react-hook-form";
+import useFetch from "../../../hooks/useFetch";
+import Loader from "../../../components/common/Loader";
+import Error from "../../../components/common/Error";
 
 export default function Profile() {
+  const { data, loading, error } = useFetch(designerService.get);
+  const [profileData, setProfileData] = useState(null);
+  const { register } = useForm();
+
+  // Fetch designers profile data
+  (useEffect(() => {
+    if (data) {
+      setProfileData(data);
+    }
+  }),
+    [data]);
+
+  if (loading) return <Loader />;
+  if (error) return <Error error={error} />;
+
   return (
     <div className="container-fluid users-profile w-100 p-4">
       <div className="row g-2 row-cols-1 pt-4">
         <div className="col-lg-3 ps-4 col-2">
-          <img className="user-profile-img" src="/imgs/default-profile.svg" />
+          <img
+            className="user-profile-img d-lg-block d-none"
+            src="/imgs/default-profile.svg"
+          />
         </div>
         <div className="col-lg-9 p-4 col-12">
-          <h3 className="page-title pb-5">Profile</h3>
+          <img
+            className="user-profile-img d-lg-none d-block pb-3 mx-auto"
+            src="/imgs/default-profile.svg"
+          />
+          <h3 className="page-title pb-lg-5 pb-3 text-center text-lg-start">
+            Profile
+          </h3>
           <hr className="pb-3"></hr>
           <form method="PUT" className="row g-3 pt-3">
             <div className="col-md-6 mb-1">
@@ -18,12 +47,14 @@ export default function Profile() {
                 <small>FIRST NAME</small>
               </label>
               <input
+                {...register("firstName")}
                 type="text"
                 className="signup-form form-control"
                 autoComplete="given-name"
                 aria-describedby="firstNameError"
                 id="firstName"
                 name="firstName"
+                value={profileData?.firstName}
               />
             </div>
             <div className="col-md-6 mb-1">
@@ -31,12 +62,14 @@ export default function Profile() {
                 Last Name
               </label>
               <input
+                {...register("lastName")}
                 type="text"
                 className="signup-form form-control"
                 autoComplete="family-name"
                 aria-describedby="lastnameError"
                 id="lastName"
                 name="lastName"
+                value={profileData?.lastName}
               />
             </div>
             <div className="col-md-6 mb-1">
@@ -44,12 +77,14 @@ export default function Profile() {
                 Email Address
               </label>
               <input
+                {...register("emailAddress")}
                 type="email"
                 className="signup-form form-control"
                 aria-describedby="emailError"
                 autoComplete="email"
                 id="emailAddress"
                 name="emailAddress"
+                value={profileData?.emailAddress}
               />
             </div>
             <div className="col-md-6 mb-1">
@@ -57,12 +92,14 @@ export default function Profile() {
                 Phone Number
               </label>
               <input
+                {...register("phoneNumber")}
                 type="text"
                 aria-describedby="phoneNumberError"
                 className="signup-form form-control"
                 autoComplete="mobile"
                 id="phoneNumber"
                 name="phoneNumber"
+                value={profileData?.phoneNumber}
               />
             </div>
             <div className="mb-1">
@@ -70,12 +107,14 @@ export default function Profile() {
                 Bio
               </label>
               <textarea
+                {...register("bio")}
                 type="text"
                 className="signup-form form-control"
                 aria-describedby="bioError"
                 autoComplete="bio"
                 id="bio"
                 name="bio"
+                value={profileData?.bio}
               />
             </div>
             <div className="col-md-6 mb-1">
@@ -83,11 +122,13 @@ export default function Profile() {
                 Experience
               </label>
               <input
+                {...register("experience")}
                 type="text"
                 className="signup-form form-control"
                 autoComplete="new-password"
                 id="experience"
                 name="experience"
+                value={profileData?.experience}
               />
             </div>
             <div className="col-md-6 mb-1 pb-3">
@@ -95,15 +136,17 @@ export default function Profile() {
                 Location
               </label>
               <input
+                {...register("location")}
                 type="text"
                 className="signup-form form-control"
                 autoComplete="new-password"
                 id="location"
                 name="location"
+                value={profileData?.location}
               />
             </div>
             <Button
-              cn="col-lg-3 rounded-pill "
+              cn="col-lg-4 col-12 rounded-pill "
               colour="white-btn"
               text={"UPDATE PROFILE"}
               arrow="false"
