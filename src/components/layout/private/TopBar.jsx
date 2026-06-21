9e1b8100417dbdd28f791f9df56e3aeb444c2306
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-21 21:03:24 +0100

    feat: add offcanvas with the sidenav components for the mobile nav

--- src/components/layout/private/TopBar.jsx
+++ src/components/layout/private/TopBar.jsx
@@ -3,6 +3,9 @@ import "./TopBar.css";
 import Search from "./Search";
 import ProfileIcon from "./ProfileIcon";
 import Notifications from "./Notifications";
+import SideHeader from "./SideHeader";
+import SideBody from "./SideBody";
+import SideFooter from "./SideFooter";
 export default function TopBar() {
   return (
     <div className="navbar nav-topbar p-0">
@@ -12,6 +15,27 @@ export default function TopBar() {
             <a href="/user">
               <img className="user-logo m-1" src="/imgs/home-user-icon.svg" />
             </a>
+            <div
+              className="offcanvas offcanvas-start user-offcanvas p-0"
+              data-bs-scroll="true"
+              tabindex="-1"
+              id="offcanvasScrolling"
+              aria-labelledby="offcanvasScrollingLabel"
+            >
+              <div className="offcanvas-header p-0 ">
+                <SideHeader />
+                <button
+                  type="button"
+                  className="btn-close btn-close-white me-3"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body p-0">
+                <SideBody />
+                <SideFooter />
+              </div>
+            </div>
           </div>
         </div>
         <div className="d-flex justify-content-end">
@@ -19,8 +43,22 @@ export default function TopBar() {
             <Search />
             <Notifications />
           </div>
-          <div className="d-flex topbar-profile  m-0">
+          <div className="d-flex topbar-profile m-0">
             <ProfileIcon />
+          </div>
+          <div className="d-flex topbar-menu d-lg-none d-flex m-0">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasScrolling"
+              aria-controls="offcanvasScrolling"
+            >
+              <span className="navbar-toggler-icon align-content-center justify-content-center">
+                <div className="navline mb-1"></div>
+                <div className="navline"></div>
+              </span>
+            </button>
           </div>
         </div>
       </div>
