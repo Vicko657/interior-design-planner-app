9bd0f21c54331936161a3e066df221a84801e463
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-21 21:00:30 +0100

    feat: add mobile logo to the topbar

--- src/components/layout/private/TopBar.jsx
+++ src/components/layout/private/TopBar.jsx
@@ -6,13 +6,22 @@ import Notifications from "./Notifications";
 export default function TopBar() {
   return (
     <div className="navbar nav-topbar p-0">
-      <div className="container-fluid pe-0 d-flex justify-content-end">
-        <div className="d-flex p-2">
-          <Search />
-          <Notifications />
+      <div className="container-fluid pe-0">
+        <div className="mobile-nav  justify-content-start ">
+          <div className="d-lg-none d-flex">
+            <a href="/user">
+              <img className="user-logo m-1" src="/imgs/home-user-icon.svg" />
+            </a>
+          </div>
         </div>
-        <div className="d-flex topbar-profile  m-0">
-          <ProfileIcon />
+        <div className="d-flex justify-content-end">
+          <div className="d-flex p-2">
+            <Search />
+            <Notifications />
+          </div>
+          <div className="d-flex topbar-profile  m-0">
+            <ProfileIcon />
+          </div>
         </div>
       </div>
     </div>
