46147ecb4759a670612fae44659481ef2c842b89
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-22 04:22:03 +0100

    feat: add Form and Modal react-bootstrap components in Clients.jsx

--- src/pages/private/clients/Clients.jsx
+++ src/pages/private/clients/Clients.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { React, useState } from "react";
 import ClientList from "./ClientList";
 import "./Clients.css";
 import Button from "../../../components/common/Button";
 import clients from "../../../data/clients.js";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 
 export default function Clients() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="container-fluid users-clients w-100 p-4">
@@ -18,6 +23,7 @@ export default function Clients() {
               colour="outline-red-btn"
               cn="rounded-pill d-flex align-items-end ms-auto p-3 pt-2 pb-2"
               text="ADD CLIENT"
+              btnfunction={handleShow}
             ></Button>
           </div>
         </div>
@@ -27,6 +33,92 @@ export default function Clients() {
           </div>
           <div className="col-md-6"></div>
         </div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>New Client</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Notes</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              btnfunction={handleClose}
+              text="Close"
+              cn="rounded-pill d-flex align-items-end ms-auto p-3 pt-2 pb-2"
+            ></Button>
+            <Button
+              variant="primary"
+              btnfunction={handleClose}
+              text="Save"
+              cn="rounded-pill d-flex align-items-end ms-auto p-3 pt-2 pb-2"
+            ></Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </>
   );
