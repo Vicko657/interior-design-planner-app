3473b536fae62bda2d67700ead43a08e5dbb4e15
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-28 23:13:13 +0100

    fix: update select component parsing values

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
-export default function Select({ props, data, selectKey, value, select }) {
+export default function Select({
+  register,
+  optionName,
+  data,
+  selectKey,
+  value,
+  select,
+}) {
   const [selected, setSelected] = useState(" ");
   const handleChange = (event) => {
     setSelected(event.target.value);
   };
   return (
     <select
-      props="true"
+      {...register(optionName)}
       className="form-select form-control"
       value={selected}
       onChange={handleChange}
     >
       <option>{"Select " + `${select}`}</option>
       {data?.map((response) => (
-        <option key={response[selectKey]} value={response[value]}>
+        <option key={response[selectKey]} value={response[selectKey]}>
           {response[value]}
         </option>
       ))}
