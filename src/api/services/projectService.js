5deba182735a71dc2cfcfee2ce1fcb66c829210f
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-28 18:12:38 +0100

    feat: add post api call to projectService

--- src/api/services/projectService.js
+++ src/api/services/projectService.js
@@ -9,6 +9,14 @@ const projectService = {
       throw err;
     }
   },
+  post: async (clientId) => {
+    try {
+      const response = await api.post(`/api/projects/${clientId}`);
+      return response.data;
+    } catch (err) {
+      throw err;
+    }
+  },
 };
 
 export default projectService;
