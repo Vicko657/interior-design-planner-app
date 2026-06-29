f22a47b94f73405aa9f840df37ab57de9c443dfb
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-29 15:36:11 +0100

    feat: add EditProject with modal functionality in ProjectDetails page

--- src/pages/private/projects/ProjectDetails.jsx
+++ src/pages/private/projects/ProjectDetails.jsx
@@ -10,6 +10,8 @@ import projectService from "../../../api/services/projectService";
 import useFetch from "../../../hooks/useFetch";
 import Loader from "../../../components/common/Loader";
 import Error from "../../../components/common/Error";
+import Button from "../../../components/common/Button";
+import EditProject from "./EditProject";
 
 export default function ProjectDetails() {
   const params = useParams();
@@ -28,14 +30,28 @@ export default function ProjectDetails() {
   return (
     <div className="container-fluid users-projects w-100">
       <div className="header project-header row row-cols-1 p-4">
-        <div className="p-4 pb-md-4 pb-3 col-md-6 col-12">
+        <div className="p-4 pb-md-4 pb-5 col-md-6 col-12">
           <p>Project Name</p>
           <h3 className="text-left">{project.projectName}</h3>
         </div>
         <div className="d-flex justify-content-md-end align-items-end justify-content-start pt-md-4 p-4 pt-0 col-md-6 col-12 mb-auto">
-          <div className="text-start">
+          <div className="me-md-5 me-auto">
             <p className="mb-2">Status</p>
             <p className="project-status">{project.status}</p>
+          </div>
+          <div className="mb-auto">
+            <Button
+              colour="outline-btn"
+              cn="rounded-pill p-3 pt-2 pb-2 me-2 "
+              text="EDIT PROJECT"
+              btnfunction={() => setModalShow(true)}
+            ></Button>
+            <EditProject
+              id={project?.id}
+              showModal={modalShow}
+              onHide={() => setModalShow(false)}
+              data={data}
+            />
           </div>
         </div>
       </div>
