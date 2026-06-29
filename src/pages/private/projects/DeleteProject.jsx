9d92ab65bca71dcde09117845b0d6faca8525512
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-29 16:36:27 +0100

    feat: add DeleteProject button component

---
+++ src/pages/private/projects/DeleteProject.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Button from "../../../components/common/Button";
+import projectService from "../../../api/services/projectService";
+export default function DeleteProject({ id }) {
+  const [apiError, setApiError] = useState(null);
+
+  // Delete existing project
+  const handleDelete = async (e) => {
+    try {
+      setApiError(null);
+      await projectService.delete(id);
+    } catch (err) {
+      const apiErrors = err.response?.data;
+      setApiError(apiErrors || "Project removal failed");
+    }
+  };
+  return (
+    <Button
+      colour=""
+      text={"Delete"}
+      arrow="false"
+      btnfunction={handleDelete}
+      type="submit"
+      cn=""
+    ></Button>
+  );
+}
