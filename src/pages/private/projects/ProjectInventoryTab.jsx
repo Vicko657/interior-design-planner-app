c96fb7b101b202c36435179a7d459cb9c537c00c
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-03 16:03:00 +0100

    fix: remove statement from  ProjectInventoryTab

--- src/pages/private/projects/ProjectInventoryTab.jsx
+++ src/pages/private/projects/ProjectInventoryTab.jsx
@@ -17,28 +17,22 @@ export default function ProjectInventoryTab({ items }) {
     <div className="user-table card h-100">
       <div className="card-body">
         <Table
-          rows={
-            items.length > 0 ? (
-              items.map((item) => (
-                <tr>
-                  <th scope="row">{item.itemName}</th>
-                  <td className="task-project">{item.description}</td>
-                  <td className="text-center">{item.quantity}</td>
-                  <td>£{item.price}</td>
-                  <td>
-                    <span
-                      className={`${item.ordered === "yes" ? "completed" : "not-completed"} rounded-pill`}
-                    >
-                      {item.ordered}
-                    </span>
-                  </td>
-                  <td href="/">edit</td>
-                </tr>
-              ))
-            ) : (
-              <p>No items found</p>
-            )
-          }
+          rows={items.map((item) => (
+            <tr>
+              <th scope="row">{item.itemName}</th>
+              <td className="task-project">{item.description}</td>
+              <td className="text-center">{item.quantity}</td>
+              <td>£{item.price}</td>
+              <td>
+                <span
+                  className={`${item.ordered === "yes" ? "completed" : "not-completed"} rounded-pill`}
+                >
+                  {item.ordered}
+                </span>
+              </td>
+              <td href="/">edit</td>
+            </tr>
+          ))}
           headers={inventoryheaders}
         />
       </div>
