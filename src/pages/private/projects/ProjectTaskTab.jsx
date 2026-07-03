103f49f3311b7dd325c8429d37a2e3000cd6ba27
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-03 15:38:39 +0100

    fix: remove fetch functionality from ProjectTaskTab

--- src/pages/private/projects/ProjectTaskTab.jsx
+++ src/pages/private/projects/ProjectTaskTab.jsx
@@ -2,28 +2,16 @@ import React from "react";
 import "../../private/tasks/Tasks.css";
 import Table from "../../../components/common/Table";
 import { useNavigate } from "react-router-dom";
-import roomService from "../../../api/services/roomService";
-import useFetch from "../../../hooks/useFetch";
-import Loader from "../../../components/common/Loader";
-import Error from "../../../components/common/Error";
 
-export default function ProjectTaskTab({ room }) {
+export default function ProjectTaskTab({ tasks }) {
   const navigate = useNavigate();
   const taskheaders = ["Task", "Details", "Due Date", "Completed"];
-
-  const { data, loading, error } = useFetch(
-    () => roomService.getById(room),
-    [room],
-  );
-
-  if (loading) return <Loader />;
-  if (error) return <Error error={error} />;
 
   return (
     <div className="user-table card h-100">
       <div className="card-body">
         <Table
-          rows={data?.checklist.map((task) => (
+          rows={tasks.map((task) => (
             <tr key={task.id}>
               <th scope="row">{task.taskName}</th>
               <td className="task-project">{task.task}</td>
