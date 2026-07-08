fdc18fb1baa5f25f20ca36aefa275a1d03428212
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-08 18:30:49 +0100

    feat: add roomType to define rooms

---
+++ src/util/roomType.js
@@ -0,0 +1,53 @@
+const roomType = [
+  {
+    id: "HALLWAY",
+    type: "Hallway",
+    description: "Entrance and transition space",
+  },
+  {
+    id: "BATHROOM",
+    type: "Bathroom",
+    description: "Washing and sanitary facilities",
+  },
+  {
+    id: "LIVING_ROOM",
+    type: "Living Room",
+    description: "Main social and relaxation area",
+  },
+  {
+    id: "KITCHEN",
+    type: "Kitchen",
+    description: "Food preparation and cooking space",
+  },
+  {
+    id: "DINING_ROOM",
+    type: "Dining Room",
+    description: "Dedicated eating anf entertaining area",
+  },
+  {
+    id: "UTILITY_ROOM",
+    type: "Utility Room",
+    description: "Laundry and household storage space",
+  },
+  {
+    id: "BEDROOM",
+    type: "Bedroom",
+    description: "Private sleeping and rest area",
+  },
+  {
+    id: "CONSERVETORY",
+    type: "Conservetory",
+    description: "Glass enclosed garden room",
+  },
+  {
+    id: "BASEMENT",
+    type: "Basement",
+    description: "Below ground level multipurpose space",
+  },
+  {
+    id: "LOFT",
+    type: "Loft",
+    description: "Converted roof space and living area",
+  },
+];
+export default roomType;
