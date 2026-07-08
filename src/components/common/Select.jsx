dd103a8bb7a6525d92fe3839a884423e7f5e5060
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-08 21:12:54 +0100

    fix: update select component if register is null

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -7,24 +7,44 @@ export default function Select({
   selectKey,
   value,
   select,
+  cn,
+  onChange,
 }) {
   const [selected, setSelected] = useState(" ");
   const handleChange = (event) => {
     setSelected(event.target.value);
   };
-  return (
-    <select
-      {...register(optionName)}
-      className="form-select form-control"
-      value={selected}
-      onChange={handleChange}
-    >
-      <option>{"Select " + `${select}`}</option>
-      {data?.map((response) => (
-        <option key={response[selectKey]} value={response[selectKey]}>
-          {response[value]}
-        </option>
-      ))}
-    </select>
-  );
+
+  if (optionName !== null && register !== null) {
+    return (
+      <select
+        {...register(optionName)}
+        className="form-select form-control"
+        value={selected}
+        onChange={handleChange}
+      >
+        <option>{"Select " + `${select}`}</option>
+        {data?.map((response) => (
+          <option key={response[selectKey]} value={response[selectKey]}>
+            {response[value]}
+          </option>
+        ))}
+      </select>
+    );
+  } else {
+    return (
+      <select
+        className={`form-select form-control ${cn}`}
+        value={selected}
+        onChange={(e) => onChange(event.target.value)}
+      >
+        <option>{"Select " + `${select}`}</option>
+        {data?.map((response) => (
+          <option key={response[selectKey]} value={response[selectKey]}>
+            {response[value]}
+          </option>
+        ))}
+      </select>
+    );
+  }
 }
