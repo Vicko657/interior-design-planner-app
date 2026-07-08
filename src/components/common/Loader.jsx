2ebde524a713d57faeacf8a88f7868a97337fcba
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-08 18:32:15 +0100

    feat: add spinner to Loader

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -3,8 +3,15 @@ import "./Loader.css";
 
 export default function Loader() {
   return (
-    <div className="d-flex justify-content-center align-items-center loader">
-      <h3>Loading...</h3>
+    <div
+      className="d-flex justify-content-center align-items-center loader"
+      role="status"
+    >
+      <div className="spinner-border ">
+        <span className="visually-hidden">
+          <h3>Loading...</h3>
+        </span>
+      </div>
     </div>
   );
 }
