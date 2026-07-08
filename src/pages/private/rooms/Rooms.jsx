cbd4ad67af296d2bfa910b9dd4ba02a083cfab47
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-08 19:47:12 +0100

    feat: add cards for each room with styling

--- src/pages/private/rooms/Rooms.jsx
+++ src/pages/private/rooms/Rooms.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Rooms.css";
 import Button from "../../../components/common/Button";
 import Card from "react-bootstrap/Card";
-import rooms from "../../../data/rooms.js";
+import roomType from "../../../util/roomType.js";
+roomType;
 
 export default function Room() {
   return (
     <>
       <div className="container-fluid users-rooms w-100 p-4">
-        <div className="row g-2 row-cols-1 pt-4 pb-4">
+        <div className="row g-2 row-cols-1 pt-4">
           <div className="col-8 ps-4">
             <h3 className="page-title">Rooms</h3>
           </div>
@@ -17,21 +18,32 @@ export default function Room() {
               page="projects"
               colour="outline-red-btn"
               cn="rounded-pill d-flex align-items-end ms-auto p-3 pt-2 pb-2"
-              text="VIEW PROJECTS"
+              text="PROJECTS"
             ></Button>
           </div>
         </div>
-        <div className="room-categories row g-2 row-cols-1 pt-4">
-          {rooms.map((room) => (
-            <Card className="ms-2 me-2" style={{ width: "18rem" }}>
-              <Card.Body className="p-5">
-                <Card.Title className="mb-3">{room.type}</Card.Title>
-                <Card.Subtitle className="mb-2 me-auto text-end">
-                  1
+        <div
+          className="room-categories row g-2 row-cols-1 p-5"
+          style={{ width: "100%" }}
+        >
+          {roomType.map((room) => (
+            <Card
+              key={room.id}
+              className="m-1"
+              style={{
+                width: "13rem",
+              }}
+            >
+              <Card.Body className="p-4">
+                <Card.Title>{room.type}</Card.Title>
+                <Card.Subtitle className="mt-2">
+                  {room.description}
                 </Card.Subtitle>
               </Card.Body>
             </Card>
           ))}
+          <hr className="mt-4"></hr>
+          <div className="room-categories row g-2 row-cols-1  "></div>
         </div>
       </div>
     </>
