026ea6607edb950c5bd9bdb0a377b1bb815c89e3
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-13 21:06:33 +0100

    feat: add patch api call in itemService

--- src/api/services/itemService.js
+++ src/api/services/itemService.js
@@ -11,5 +11,16 @@ const itemService = {
       throw err;
     }
   },
+  put: async (itemData, roomId, index) => {
+    try {
+      const response = await api.patch(
+        `/api/rooms/${roomId}/inventory/${index}`,
+        itemData,
+      );
+      return response.data;
+    } catch (err) {
+      throw err;
+    }
+  },
 };
 export default itemService;
