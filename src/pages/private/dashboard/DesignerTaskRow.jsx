8e58a2446eb0b4f151cbdafaf9e842fbab2a3112
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-13 19:54:02 +0100

    fix: update recent task table in dashboard

--- src/pages/private/dashboard/DesignerTaskRow.jsx
+++ src/pages/private/dashboard/DesignerTaskRow.jsx
@@ -3,30 +3,26 @@ import "../../../components/common/Table.css";
 import "../tasks/Tasks.css";
 import { useNavigate } from "react-router-dom";
 
-export default function DesignerTaskRow({ tasks, key }) {
+export default function DesignerTaskRow({ task }) {
   const navigate = useNavigate();
-  const totalTasks = tasks.length;
+  const totalTasks = length;
   return (
     <tr
-      key={key}
-      onClick={() => navigate(`project/${tasks.project}`)}
+      onClick={() => navigate(`project/${task.project}`)}
       style={{ cursor: "pointer" }}
     >
-      <th scope="row">{tasks.taskName}</th>
+      <th scope="row">{task.taskName}</th>
 
       <td>
         <span
-          className={`${tasks.isCompleted === true ? "completed" : "not-completed"} rounded-pill`}
+          className={`${task.isCompleted === true ? "completed" : "not-completed"} rounded-pill`}
         >
-          {tasks.isCompleted}
+          {task.isCompleted === true ? "YES" : "NO"}
         </span>
       </td>
 
-      <td
-        className="p-2 task-project text-center project-category"
-        rowSpan={totalTasks}
-      >
-        <span className="rounded-pill project-category">{tasks.project}</span>
+      <td className="p-2 task-project text-center project-category">
+        <span className="rounded-pill project-category">{task.project}</span>
       </td>
     </tr>
   );
