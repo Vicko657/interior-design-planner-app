7646a383e6972d2c7afb62453449373c8db64f37
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-13 19:25:48 +0100

    feat: add Edit button and modal to task row

--- src/pages/private/projects/ProjectTaskTab.jsx
+++ src/pages/private/projects/ProjectTaskTab.jsx
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../private/tasks/Tasks.css";
 import Table from "../../../components/common/Table";
 import { useNavigate } from "react-router-dom";
+import EditTask from "../tasks/EditTask";
+import Button from "../../../components/common/Button";
 
-export default function ProjectTaskTab({ tasks }) {
+export default function ProjectTaskTab({ tasks, room }) {
   const navigate = useNavigate();
   const taskheaders = ["Task", "Details", "Due Date", "Completed"];
-
+  const [modalShow, setModalShow] = useState(false);
+  const [selected, setSelected] = useState(null);
   return (
     <div className="user-table card h-100">
       <div className="card-body">
         <Table
-          rows={tasks.map((task) => (
-            <tr key={task.id}>
+          rows={tasks.map((task, index) => (
+            <tr key={index}>
               <th scope="row">{task.taskName}</th>
               <td className="task-project">{task.task}</td>
               <td>{task.date}</td>
               <td>
                 <span
-                  className={`${task.completed === "yes" ? "completed" : "not-completed"} rounded-pill`}
+                  className={`${task.completed === true ? "completed" : "not-completed"} rounded-pill`}
                 >
-                  {task.completed}
+                  {task.completed === true ? "YES" : "NO"}
                 </span>
               </td>
-              <td href="/">edit</td>
+              <td onClick={(e) => e.stopPropagation()}>
+                <Button
+                  arrow="false"
+                  text="Edit"
+                  btnfunction={() => {
+                    setSelected(index);
+                    setModalShow(true);
+                  }}
+                  type="submit"
+                  cn="delete-btn"
+                ></Button>
+                <EditTask
+                  id={room}
+                  index={index}
+                  showModal={modalShow && selected === index}
+                  onHide={() => setModalShow(false)}
+                  response={task}
+                />
+              </td>
             </tr>
           ))}
           headers={taskheaders}
