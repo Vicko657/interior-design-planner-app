88b2ceab0217d9c598924a41a1c67fecc9fa14e1
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-13 20:56:11 +0100

    fix: update path for post api call in itemService

--- src/api/services/itemService.js
+++ src/api/services/itemService.js
@@ -2,7 +2,10 @@ import api from "../AxiosConfig";
 const itemService = {
   post: async (itemData, roomId) => {
     try {
-      const response = await api.patch(`/api/rooms/${roomId}/item`, itemData);
+      const response = await api.patch(
+        `/api/rooms/${roomId}/inventory`,
+        itemData,
+      );
       return response.data;
     } catch (err) {
       throw err;
