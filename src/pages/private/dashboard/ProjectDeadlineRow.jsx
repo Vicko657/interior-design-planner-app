a8711ded8d927bf8eb37844e80898b35f5935275
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-22 16:27:29 +0100

    fix: match status enums values to projectStatus.js in ProjectDeadlineRow

--- src/pages/private/dashboard/ProjectDeadlineRow.jsx
+++ src/pages/private/dashboard/ProjectDeadlineRow.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "../../../components/common/Table.css";
 import { useNavigate } from "react-router-dom";
+import projectStatus from "../../../util/projectStatus.js";
 
 export default function ProjectDeadlineRow({ project }) {
   const navigate = useNavigate();
+  const status = projectStatus.find((s) => s.id === project.status);
   return (
     <tr
       onClick={() => navigate(`${project.projectName}`)}
@@ -11,7 +13,7 @@ export default function ProjectDeadlineRow({ project }) {
     >
       <th scope="row">{project.projectName}</th>
       <td>
-        <span className="status rounded-pill">{project.status}</span>
+        <span className="status rounded-pill">{status?.status}</span>
       </td>
       <td>{project.date}</td>
     </tr>
