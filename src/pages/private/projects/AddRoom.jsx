cf4cc6fd442c2d06c309a9a3516c14b95d93e9a3
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-22 16:04:58 +0100

    fix: remove object and replace with roomType.js in AddRoom

--- src/pages/private/projects/AddRoom.jsx
+++ src/pages/private/projects/AddRoom.jsx
@@ -7,20 +7,10 @@ import useFetch from "../../../hooks/useFetch";
 import Select from "../../../components/common/Select";
 import Loader from "../../../components/common/Loader";
 import Error from "../../../components/common/Error";
+import roomType from "../../../util/roomType.js";
+roomType;
 
 export default function AddRoom({ project, showModal, onHide }) {
-  const roomType = [
-    { id: "HALLWAY", type: "Hallway" },
-    { id: "BATHROOM", type: "Bathroom" },
-    { id: "LIVING_ROOM", type: "Living Room" },
-    { id: "KITCHEN", type: "Kitchen" },
-    { id: "DINING_ROOM", type: "Dining Room" },
-    { id: "UTILITY_ROOM", type: "Utility Room" },
-    { id: "BEDROOM", type: "Bedroom" },
-    { id: "CONSERVETORY", type: "Conservetory" },
-    { id: "BASEMENT", type: "Basement" },
-    { id: "LOFT", type: "Loft" },
-  ];
   const units = [
     { id: "m", unit: "Meters" },
     { id: "cm", unit: "Centimeters" },
